845b39d375d0cbcabeb6f4bb6450c0bca1a7d56b
Biel Llobera <biel_llobera@dkl.digital>
2024-01-15 13:25:41 +0100

    fixed mypy errors

--- src/airflow_tools/data_lake_facade.py
+++ src/airflow_tools/data_lake_facade.py
@@ -1,26 +1,29 @@
 import logging
 from io import BytesIO
+from typing import Union
 
 from airflow.providers.amazon.aws.hooks.base_aws import AwsGenericHook
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.microsoft.azure.hooks.wasb import WasbHook
 
-DataLakeConnection = WasbHook | S3Hook | AwsGenericHook
+DataLakeConnection = Union[WasbHook, S3Hook, AwsGenericHook]
 logger = logging.getLogger(__file__)
 
 
 class DataLakeFacade:
     """Provides a consistent interface over different Data Lakes (S3/Blob Storage etc.)"""
 
-    def __init__(self, conn: DataLakeConnection):
-        self.conn = conn
-        if isinstance(self.conn, AwsGenericHook) and not isinstance(self.conn, S3Hook):
-            self.conn = S3Hook(self.conn.aws_conn_id)
+    def __init__(self, conn: Union[WasbHook, S3Hook, AwsGenericHook]):
+        self.conn: S3Hook | WasbHook = (
+            S3Hook(conn.aws_conn_id)
+            if isinstance(conn, AwsGenericHook) and not isinstance(conn, S3Hook)
+            else conn
+        )
 
     def write(self, data: str | bytes | BytesIO, path: str):
         match self.conn.conn_type:
             case "wasb":
-                self.conn: WasbHook
+                assert isinstance(self.conn, WasbHook)
                 container_name, blob_name = _get_container_and_blob_name(path)
                 if isinstance(data, str):
                     self.conn.load_string(data, container_name, blob_name)
@@ -35,7 +38,7 @@ class DataLakeFacade:
                     container_name=container_name, blob_name=blob_name, data=data
                 )
             case "aws":
-                self.conn: S3Hook
+                assert isinstance(self.conn, S3Hook)
                 bucket_name, key_name = _get_bucket_and_key_name(path)
                 if isinstance(data, str):
                     self.conn.load_string(data, key_name, bucket_name)
@@ -51,13 +54,13 @@ class DataLakeFacade:
     def delete_prefix(self, prefix: str):
         match self.conn.conn_type:
             case "wasb":
-                self.conn: WasbHook
+                assert isinstance(self.conn, WasbHook)
                 container_name, blob_prefix = _get_container_and_blob_name(prefix)
                 blobs = self.conn.get_blobs_list(container_name, prefix=blob_prefix)
                 for blob in blobs:
-                    self.conn.delete_blob(container_name, blob.name)
+                    self.conn.delete_blobs(container_name, blob.name)
             case "aws":
-                self.conn: S3Hook
+                assert isinstance(self.conn, S3Hook)
                 bucket_name, key_prefix = _get_bucket_and_key_name(prefix)
                 self.conn.get_bucket(bucket_name).objects.filter(
                     Prefix=key_prefix
