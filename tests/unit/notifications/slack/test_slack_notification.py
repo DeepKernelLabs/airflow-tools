bbe294d4e34f083bb9f546bae28f5b5fa1bf199b
Víctor Centelles <152852247+victor-centelles-dkl@users.noreply.github.com>
2024-02-09 11:43:23 +0100

    feat[notifications]: [RET-158] add Slack notifications for failing dags (by Incoming Webhooks) (#19)
    * feat[notifications]: add Slack notifications for failing dags (by Incoming Webhooks)
    
    The idea is provide a easy way for notify failures in dags via Slack
    using the Incoming Webhooks.
    
    New Slack connection has to be created on Airflow for using with
    `SLACK_WEBHOOK_NOTIFICATION_CONN` name via webserver (or using
    `AIRFLOW_CONN_SLACK_WEBHOOK_NOTIFICATION_CONN` environment variable).
    
    The dags has to use the `dag_failure_slack_notification_webhook` method
    (`notifications.slack.webhook`) on `on_failure_callback` property. This
    method allows to change the default parameters (`text`, `blocks`,
    `include_bloks` and `image_url`).
    
    * More info about Slack Incoming Webhooks: https://api.slack.com/messaging/webhooks
    
    * Add pytest-mock (poetry test group)
    
    * Add example in README for using Slack notifications
    
    * Add a different default msg for dags and tasks
    
    Introduce the `property` source in
    `dag_failure_slack_notification_webhook` function for indicating if this
    method is used for sending a messahe from a Dag or a task. Only `DAG`
    and `TASK` sources are valid.
    
    ---------
    
    Co-authored-by: Víctor Centelles <victor.centelles@gmail.com>

---
+++ tests/unit/notifications/slack/test_slack_notification.py
@@ -0,0 +1,41 @@
+import pytest
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+from airflow_tools.notifications.slack import webhook as slack_webhook
+
+
+def test_webhook_slack_notifications_on_fail(mocker):
+    slack_mock = mocker.patch.object(
+        slack_webhook, 'dag_failure_slack_notification_webhook'
+    )
+
+    with DAG('test_slack_notification_on_fail_dag') as dag:
+        BashOperator(
+            task_id='failing_task',
+            bash_command='exit 1',
+            on_failure_callback=slack_webhook.dag_failure_slack_notification_webhook(),
+        )
+
+    dag.test()
+
+    slack_mock.assert_called_once_with()
+
+
+@pytest.mark.parametrize(
+    'source, expected_text',
+    [
+        (
+            slack_webhook.Source.DAG,
+            "{% for t in dag_run.get_task_instances(state='failed') %}'{{ t.task_id }}', {% endfor %}",
+        ),
+        (slack_webhook.Source.TASK, "{{ task.task_id }}"),
+        ('unkown source', '"unknown source"'),
+    ],
+)
+def test_webhook_block_blocks_format_by_source(source: str, expected_text: str):
+    blocks = slack_webhook._get_message_blocks(
+        text=f'test_{source}', image_url='http://localhost/image.png', source=source
+    )
+    failed_task_info = blocks[0]['text']['text']
+    assert expected_text in failed_task_info
