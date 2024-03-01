8a8fd3f9ed049a54a8233a409f60657bc51de0a0
pedro-medina-riesgo-dkl <pedro_medina@dkl.digital>
2024-03-01 12:54:47 +0100

    feat(operators): Add SQLToFilesystem operator

--- src/airflow_tools/providers/filesystem/operators/filesystem.py
+++ src/airflow_tools/providers/filesystem/operators/filesystem.py
@@ -2,7 +2,9 @@ from typing import Optional, Protocol
 
 from airflow.hooks.base import BaseHook
 from airflow.models import BaseOperator
+from airflow.providers.common.sql.hooks.sql import DbApiHook
 
+from airflow_tools.data_lake_facade import DataLakeFacade
 from airflow_tools.filesystems.filesystem_factory import FilesystemFactory
 
 
@@ -26,7 +28,7 @@ class FilesystemToFilesystem(BaseOperator):
         destination_path: str,
         data_transformation: Optional[Transformation] = None,
         *args,
-        **kwargs
+        **kwargs,
     ) -> None:
         super().__init__(*args, **kwargs)
         self.source_fs_conn_id = source_fs_conn_id
@@ -53,3 +55,57 @@ class FilesystemToFilesystem(BaseOperator):
             else self.destination_path
         )
         destination_fs_hook.write(data, full_destination_path)
+
+
+class SQLToFilesystem(BaseOperator):
+    """
+    Copies data from a SQL query to a filesystem.
+    """
+
+    template_fields = (
+        'source_name',
+        'source_schema_name',
+        'source_table_name',
+        'destination_path',
+    )
+
+    def __init__(
+        self,
+        source_sql_conn_id: str,
+        destination_fs_conn_id: str,
+        source_name: str,
+        source_schema_name: str,
+        source_table_name: str,
+        destination_path: str,
+        batch_size: Optional[int] = 100000,
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.source_sql_conn_id = source_sql_conn_id
+        self.destination_fs_conn_id = destination_fs_conn_id
+        self.source_name = source_name
+        self.source_schema_name = source_schema_name
+        self.source_table_name = source_table_name
+        self.destination_path = destination_path
+        self.batch_size = batch_size
+
+    def execute(self, context):
+        source_sql_hook: DbApiHook = BaseHook.get_connection(
+            self.source_sql_conn_id
+        ).get_hook()
+        source_sql_hook.__schema__ = self.source_schema_name
+
+        data_lake_conn = BaseHook.get_connection(self.destination_fs_conn_id)
+        data_lake_facade = DataLakeFacade(conn=data_lake_conn.get_hook())
+
+        query = f'SELECT * FROM {self.source_table_name};'
+        for i, df in enumerate(
+            source_sql_hook.get_pandas_df_by_chunks(
+                sql=query, chunksize=self.batch_size
+            ),
+            start=1,
+        ):
+            generated_path = f"{self.source_name.lower()}/{self.source_table_name.lower()}/part{i:04}.parquet"
+            full_file_path = f"{self.destination_path.rstrip('/')}/{generated_path}"
+            data_lake_facade.write(df.to_parquet(index=False), full_file_path)
